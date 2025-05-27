4650bc9b5962c9c9e4110b4215121572dcd612f1
arkaizn <silas.gubler@hispeed.ch>
2025-05-28 00:36:39 +0200

    press esc for close

--- files/popup.py
+++ files/popup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-import os, sys
+import os
+import sys
 
-# make sure style.css is always found, no matter who exec’s us
+# Ensure style.css is found regardless of how the script is launched
 script_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(script_dir)
 
@@ -9,101 +10,80 @@ import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
 from gi.repository import Gtk, GLib, Gdk
+from gi.repository.Gdk import KEY_Escape
 
 class HyprlandHelperApp(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self, title="Hyprland Helper")
+        super().__init__(title="Hyprland Helper")
+        # Close on Escape
+        self.connect("key-press-event", self.on_key_press)
         self.set_default_size(400, 300)
-        
-        # Load CSS before creating widgets
+
+        # Load any custom overrides (only if style.css exists)
         self.load_css()
-        
+
         # Main container
         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
         self.add(box)
-        
-        # Header - add CSS class
+
+        # Header label
         header = Gtk.Label()
         header.set_markup("<big><b>Hyprland Helper</b></big>")
         header.get_style_context().add_class("header-label")
         box.pack_start(header, False, False, 10)
-        
-        # Buttons
+
+        # Buttons container
         buttons_box = Gtk.Box(spacing=5)
         box.pack_start(buttons_box, False, False, 0)
-        
-        # Reload Hyprland button
+
+        # Reload button
         reload_btn = Gtk.Button(label="Reload Hyprland")
         reload_btn.connect("clicked", self.on_reload_clicked)
         buttons_box.pack_start(reload_btn, True, True, 0)
-        
+
         # Screenshot button
         screenshot_btn = Gtk.Button(label="Take Screenshot")
         screenshot_btn.connect("clicked", self.on_screenshot_clicked)
         buttons_box.pack_start(screenshot_btn, True, True, 0)
-        
-        # Status area - add CSS class
+
+        # Status label
         self.status = Gtk.Label(label="Ready")
         self.status.get_style_context().add_class("status-label")
         box.pack_start(self.status, False, False, 10)
-        
+
         # Quit button
         quit_btn = Gtk.Button(label="Quit")
         quit_btn.connect("clicked", Gtk.main_quit)
         box.pack_start(quit_btn, False, False, 10)
-    
+
     def on_reload_clicked(self, button):
         self.status.set_text("Reloading Hyprland...")
         GLib.spawn_command_line_async("hyprctl reload")
         self.status.set_text("Hyprland reloaded!")
-    
+
     def on_screenshot_clicked(self, button):
         self.status.set_text("Taking screenshot...")
         GLib.spawn_command_line_async("grim ~/screenshot-$(date +'%Y%m%d-%H%M%S').png")
         self.status.set_text("Screenshot saved in home directory!")
-    
+
+    def on_key_press(self, widget, event):
+        if event.keyval == KEY_Escape:
+            Gtk.main_quit()
+
     def load_css(self):
-        css = b"""
-        window {
-            background-color: #2d2d2d;
-        }
-        label {
-            color: #ffffff;
-        }
-        button {
-            background-color: #3c3c3c;
-            color: #ffffff;
-            border-radius: 5px;
-        }
-        button:hover {
-            background-color: #4c4c4c;
-        }
-        .header-label {
-            font-size: 18px;
-            font-weight: bold;
-        }
-        .status-label {
-            font-style: italic;
-        }
-        """
-        
-        # Try to load from file first, fall back to embedded CSS
-        try:
-            css_provider = Gtk.CssProvider()
-            css_provider.load_from_path("style.css")
-        except GLib.Error:
-            css_provider = Gtk.CssProvider()
-            css_provider.load_from_data(css)
-        
-        screen = Gdk.Screen.get_default()
-        style_context = Gtk.StyleContext()
-        style_context.add_provider_for_screen(
-            screen,
-            css_provider,
+        css_path = os.path.join(script_dir, 'style.css')
+        if not os.path.exists(css_path):
+            return
+        provider = Gtk.CssProvider()
+        provider.load_from_path(css_path)
+        Gtk.StyleContext.add_provider_for_screen(
+            Gdk.Screen.get_default(),
+            provider,
             Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
         )
 
-win = HyprlandHelperApp()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+if __name__ == '__main__':
+    win = HyprlandHelperApp()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
